e8f21b104934732e1d92cef979ca3e62fd0daea3
jocieljesus <jocielalvesdejesus@gmail.com>
2026-07-23 17:45:25 -0300

    tela de visualizacao de estoque~

--- src/main/java/com/jociel/estoque/EstoqueController.java
+++ src/main/java/com/jociel/estoque/EstoqueController.java
@@ -1,4 +1,26 @@
 package com.jociel.estoque;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+
 public class EstoqueController {
+
+
+    @FXML
+    protected void  aoVoltarMenu(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
 }
--- src/main/java/com/jociel/estoque/MenuController.java
+++ src/main/java/com/jociel/estoque/MenuController.java
@@ -6,10 +6,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import java.awt.*;
 import java.io.IOException;
-
 public class MenuController {
+
+
+    @FXML
+    protected void  aoVisualizarEstoque(ActionEvent event) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("estoque.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Estoque de Produto");
+        stage.show();
+    }
+
+    @FXML
+    protected void  aoCadastrarProduto(ActionEvent event){
+
+    }
+
+    @FXML
+    protected  void aoGerarRelatorio(ActionEvent event){
+
+    }
 
     @FXML
     protected void aoSair(ActionEvent event) throws IOException {
@@ -20,5 +41,5 @@ public class MenuController {
         stage.setScene(scene);
         stage.show();
     }
-}
 
+}
